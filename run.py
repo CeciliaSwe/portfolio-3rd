78e82da1b8eb81b7fed3f9cc5495b4122daa0ea4
CeciliaSwe <cedergren.cecilia@gmail.com>
2021-10-07 13:09:46 +0000

    Add user_choice function to request input on task to perform

--- run.py
+++ run.py
@@ -12,6 +12,24 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('doc_tracking')
 
-
 print("Welcome to Document Status Tracking!")
-print("What would you like to do today?")
+
+
+def user_choice():
+    """
+    Request the user to choose between new, status and update
+    """
+    while True:
+        print("What would you like to do?\n")
+        user_input = input("Enter new/status/update: \n")
+
+        if validate_user_choice(user_input):
+            print("Thank you!")
+            break
+
+    return user_input
+
+user_choice()
+
+
+
